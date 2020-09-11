554ac984d0d1e66d5d0aaa8dcfe6085093eafca5
Henrik Jangefelt <henke9105@hotmail.com>
2020-09-11 23:51:00 +0200

    Created Toy class

---
+++ SantasWorkshop/SantasWorkshop/Models/Toy.cpp
@@ -0,0 +1,15 @@
+//
+//  Toy.cpp
+//  SantasWorkshop
+//
+//  Created by Henrik Jangefelt on 2020-09-11.
+//  Copyright © 2020 Henrik Jangefelt Nilsson. All rights reserved.
+//
+
+#include "Toy.hpp"
+
+namespace santas_workshop {
+
+int Toy::s_idGenerator{ 1 };
+
+}
---
+++ SantasWorkshop/SantasWorkshop/Models/Toy.hpp
@@ -0,0 +1,39 @@
+//
+//  Toy.hpp
+//  SantasWorkshop
+//
+//  Created by Henrik Jangefelt on 2020-09-11.
+//  Copyright © 2020 Henrik Jangefelt Nilsson. All rights reserved.
+//
+
+#ifndef Toy_hpp
+#define Toy_hpp
+
+#include <stdio.h>
+#include <string>
+
+namespace santas_workshop {
+
+class Toy
+{
+private:
+    std::string m_name;
+    int m_manufacturingTimeInDays;
+    int m_id;
+    static int s_idGenerator;
+    
+public:
+    Toy(const std::string &name, int time)
+        : m_name{ name }, m_manufacturingTimeInDays{ time }, m_id{ s_idGenerator++ }
+    {
+    }
+    
+    
+    const std::string& getName() const { return m_name; }
+    int getId() { return m_id; }
+    int getManufacturingTime() const { return m_manufacturingTimeInDays; }
+};
+
+}
+
+#endif /* Toy_hpp */
