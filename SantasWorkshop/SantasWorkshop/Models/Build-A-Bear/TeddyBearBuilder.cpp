8041591ac928b2dd034a8981670823f8951b38dd
Henrik Jangefelt <henke9105@hotmail.com>
2020-09-27 16:50:31 +0200

    Skeleton for TeddyBear builder class

---
+++ SantasWorkshop/SantasWorkshop/Models/Build-A-Bear/TeddyBearBuilder.cpp
@@ -0,0 +1,15 @@
+//
+//  TeddyBearBuilder.cpp
+//  SantasWorkshop
+//
+//  Created by Henrik Jangefelt on 2020-09-22.
+//  Copyright © 2020 Henrik Jangefelt Nilsson. All rights reserved.
+//
+
+#include "TeddyBearBuilder.hpp"
+
+namespace santas_workshop {
+
+
+
+}
---
+++ SantasWorkshop/SantasWorkshop/Models/Build-A-Bear/TeddyBearBuilder.hpp
@@ -0,0 +1,24 @@
+//
+//  TeddyBearBuilder.hpp
+//  SantasWorkshop
+//
+//  Created by Henrik Jangefelt on 2020-09-22.
+//  Copyright © 2020 Henrik Jangefelt Nilsson. All rights reserved.
+//
+
+#ifndef TeddyBearBuilder_hpp
+#define TeddyBearBuilder_hpp
+
+#include <stdio.h>
+
+namespace santas_workshop {
+
+class TeddyBearBuilder
+{
+    
+};
+
+
+}
+
+#endif /* TeddyBearBuilder_hpp */
